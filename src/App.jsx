f2f23bb6cfa057a7386f30188de0a897817370af
Oskar <oskarkaklewski@gmail.com>
2024-06-16 14:13:45 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState, Suspense } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+
+      <div className='App'>
+
+
+      </div>
+    </Suspense>
+  )
+}
+
+export default App
