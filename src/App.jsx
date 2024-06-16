c30d88500f6dba8379b06fd813893639c11b26c9
Oskar <oskarkaklewski@gmail.com>
2024-06-16 14:17:09 +0200

    Added Error Boundary component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import { useState, Suspense } from 'react'
-
+import { useState, Suspense } from 'react';
+import ErrorBoundary from './ErrorBoundary';
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
 
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-
-      <div className='App'>
-
-
-      </div>
-    </Suspense>
-  )
+	return (
+		<ErrorBoundary fallback={<div>Error...</div>}>
+			<Suspense fallback={<div>Loading...</div>}>
+				<div className='App'></div>
+			</Suspense>
+		</ErrorBoundary>
+	);
 }
 
-export default App
+export default App;
