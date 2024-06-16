b5cee718eda122a688f7ae9965931d65f644ea05
Oskar <oskarkaklewski@gmail.com>
2024-06-16 14:31:41 +0200

    Added the url fetching function

--- src/components/PokemonGrid.jsx
+++ src/components/PokemonGrid.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { use } from 'react';
 
-export default function PokemonCard(){
-  return(
-    <div>PokemonGrid</div>
-  )
+async function fetchData(url) {
+	const response = await fetch(url);
+	return response.json();
+}
+
+export default function PokemonCard() {
+	url = 'https://jsonplaceholder.typicode.com/todos/1';
+	const data = use(fetchData(url));
+	return <div>{data.title}</div>;
 }
