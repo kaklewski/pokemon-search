10acce9a5dbabc3c098e913d7cbad1267f3ead2d
Oskar <oskarkaklewski@gmail.com>
2024-06-16 21:39:58 +0200

    Added a new logo

--- src/components/PokemonGrid.jsx
+++ src/components/PokemonGrid.jsx
@@ -1,5 +1,6 @@
 import React, { use, useState } from 'react';
 import styles from './pokemonGrid.module.css';
+import logo from '../assets/logo.png';
 
 async function fetchData(url) {
 	const response = await fetch(url);
@@ -20,27 +21,33 @@ export default function PokemonGrid(props) {
 
 	return (
 		<div className={styles.pokemonGrid}>
-			<h1 className={styles.header}>MY POKEMON</h1>
+			<h1 className={styles.header}>
+				<img src={logo} />
+			</h1>
 			<div className={styles.listContainer}>
 				<input
+					className={styles.search}
 					placeholder='Search Pokemon'
 					value={search}
 					onChange={(e) => setSearch(e.target.value)}
 				/>
-				{data.results
-					.filter((value) => {
-						return value.name.includes(search);
-					})
-					.map((pokemon, pokemonIndex) => {
-						return (
-							<div
-								onClick={handleSelectPokemon(pokemon.name)}
-								key={pokemonIndex}
-								className={styles.pokemon}>
-								{pokemon.name}
-							</div>
-						);
-					})}
+				<div>
+					{data.results
+						.filter((value) => {
+							return value.name.includes(search);
+						})
+						.map((pokemon, pokemonIndex) => {
+							return (
+								<div
+									onClick={handleSelectPokemon(pokemon.name)}
+									key={pokemonIndex}
+									className={styles.pokemon}>
+									{pokemon.name.charAt(0).toUpperCase() +
+										pokemon.name.slice(1)}
+								</div>
+							);
+						})}
+				</div>
 			</div>
 		</div>
 	);
