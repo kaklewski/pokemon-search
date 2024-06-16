709a7035ac276679bbef321598c5aacfb839f276
Oskar <oskarkaklewski@gmail.com>
2024-06-16 15:48:45 +0200

    Added displaying Pokemon grid or card

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState, Suspense } from 'react';
 import ErrorBoundary from './ErrorBoundary';
+import PokemonCard from './components/PokemonCard';
+import PokemonGrid from './components/PokemonGrid';
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [selectedPokemon, setSelectedPokemon] = useState(null);
 
 	return (
 		<ErrorBoundary fallback={<div>Error...</div>}>
 			<Suspense fallback={<div>Loading...</div>}>
-				<div className='App'></div>
+				<div className='App'>
+					{selectedPokemon ? <PokemonCard /> : <PokemonGrid />}
+				</div>
 			</Suspense>
 		</ErrorBoundary>
 	);
