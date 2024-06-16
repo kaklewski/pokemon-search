f60d4281796e3fdd6c98038ed815bbe5f33f3c57
Oskar <oskarkaklewski@gmail.com>
2024-06-16 21:39:41 +0200

    Added the close button

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -16,30 +16,40 @@ export default function PokemonCard(props) {
 		<div className={styles.card}>
 			<div className={styles.headerBar}>
 				<h1>{selectedPokemon}</h1>
-				<div onClick={clearHandler}>x</div>
+				<div className={styles.closeBtn} onClick={clearHandler}>
+					<svg
+						xmlns='http://www.w3.org/2000/svg'
+						width='24'
+						height='24'
+						viewBox='0 0 24 24'
+						fill='none'
+						stroke='currentColor'
+						stroke-width='2'
+						stroke-linecap='round'
+						stroke-linejoin='round'
+						class='icon icon-tabler icons-tabler-outline icon-tabler-x'>
+						<path stroke='none' d='M0 0h24v24H0z' fill='none' />
+						<path d='M18 6l-12 12' />
+						<path d='M6 6l12 12' />
+					</svg>
+				</div>
 			</div>
 			<img src={data.sprites.front_default} alt={selectedPokemon} />
 			<h3>Stats</h3>
 			{data.stats.map((stat, statIndex) => {
 				return (
-					<div key={statIndex}>
-						<p>
-							<b>{stat.stat.name}: </b>
-							{stat.base_stat}
-						</p>
-					</div>
+					<p key={statIndex}>
+						<b>{stat.stat.name}: </b>
+						{stat.base_stat}
+					</p>
 				);
 			})}
 			<h3>Types</h3>
-			{data.types.map((type, typeIndex) => {
-				return (
-					<div key={typeIndex}>
-						<p>
-							<b>{type.type.name}</b>
-						</p>
-					</div>
-				);
-			})}
+			<div>
+				{data.types.map((type, typeIndex) => {
+					return <span key={typeIndex}>{type.type.name}, </span>;
+				})}
+			</div>
 		</div>
 	);
 }
