a698f97fe39387556ed61e905b198008d24c2f03
Oskar <oskarkaklewski@gmail.com>
2024-06-17 22:44:24 +0200

    Started UI refresh

--- src/components/PokemonGrid.jsx
+++ src/components/PokemonGrid.jsx
@@ -21,16 +21,37 @@ export default function PokemonGrid(props) {
 
 	return (
 		<div className={styles.pokemonGrid}>
-			<h1 className={styles.header}>
-				<img src={logo} />
-			</h1>
+			<div className={styles.header}>
+				<div className={styles.headerContainer}>
+					<img src={logo} />
+					<h1>Which Pokémon are you looking for?</h1>
+					<div className={styles.searchBar}>
+						<svg
+							xmlns='http://www.w3.org/2000/svg'
+							width='24'
+							height='24'
+							viewBox='0 0 24 24'
+							fill='none'
+							stroke='currentColor'
+							stroke-width='2'
+							stroke-linecap='round'
+							stroke-linejoin='round'
+							class='icon icon-tabler icons-tabler-outline icon-tabler-search'>
+							<path stroke='none' d='M0 0h24v24H0z' fill='none' />
+							<path d='M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0' />
+							<path d='M21 21l-6 -6' />
+						</svg>
+						<input
+							placeholder='e.g. Pikachu'
+							value={search}
+							onChange={(e) => setSearch(e.target.value)}
+							autofocus
+						/>
+					</div>
+				</div>
+			</div>
+
 			<div className={styles.listContainer}>
-				<input
-					className={styles.search}
-					placeholder='Search Pokemon'
-					value={search}
-					onChange={(e) => setSearch(e.target.value)}
-				/>
 				<div>
 					{data.results
 						.filter((value) => {
