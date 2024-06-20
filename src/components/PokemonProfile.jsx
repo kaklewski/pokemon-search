362c25ff55785789afb27c229d176aa28389765e
Oskar <oskarkaklewski@gmail.com>
2024-06-20 22:08:40 +0200

    Changed SVG attributes to fit React

--- src/components/PokemonProfile.jsx
+++ src/components/PokemonProfile.jsx
@@ -40,10 +40,10 @@ export default function PokemonCard(props) {
 							viewBox='0 0 24 24'
 							fill='none'
 							stroke='currentColor'
-							stroke-width='1.7'
-							stroke-linecap='round'
-							stroke-linejoin='round'
-							class='icon icon-tabler icons-tabler-outline icon-tabler-arrow-left'>
+							strokeWidth='1.7'
+							strokeLinecap='round'
+							strokeLinejoin='round'
+							>
 							<path stroke='none' d='M0 0h24v24H0z' fill='none' />
 							<path d='M5 12l14 0' />
 							<path d='M5 12l6 6' />
--- src/components/PokemonStat.jsx
+++ src/components/PokemonStat.jsx
@@ -3,7 +3,7 @@ import styles from './pokemonProfile.module.css';
 
 function countWidthPercentage(number) {
 	number = parseInt(number);
-	const percentage = Math.round((number / 154)*100).toString() + '%';
+	const percentage = Math.round((number / 154) * 100).toString() + '%';
 	return percentage;
 }
 
@@ -11,11 +11,11 @@ export default function PokemonStat(props) {
 	const { statValue, statType } = props;
 
 	return (
-		<div className={styles.statContainer} data-statType={statType}>
+		<div className={styles.statContainer} data-stat-type={statType}>
 			<div
 				className={styles.statBar}
 				style={{ width: countWidthPercentage(statValue) }}
-				data-statType={statType}>
+				data-stat-type={statType}>
 				{statValue}
 			</div>
 		</div>
