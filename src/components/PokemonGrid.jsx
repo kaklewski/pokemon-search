7ddde2dceb7e409dea7c3c96c3959a94390c0c14
Oskar <oskarkaklewski@gmail.com>
2024-06-20 21:34:40 +0200

    Finished the UI

--- src/components/PokemonGrid.jsx
+++ src/components/PokemonGrid.jsx
@@ -12,6 +12,10 @@ function capitalizeFirstLetter(word) {
 	return word.charAt(0).toUpperCase() + word.slice(1);
 }
 
+function displayId(number) {
+	return '#' + (number + 1).toString().padStart(3, '0');
+}
+
 export default function PokemonGrid(props) {
 	const { handleSelectPokemon, url } = props;
 	const [search, setSearch] = useState('');
@@ -23,8 +27,6 @@ export default function PokemonGrid(props) {
 		data = use(fetchData(url));
 		localStorage.setItem('pokemon-cards', JSON.stringify(data));
 	}
-
-	// console.log(data); // DO WYWALENIA
 
 	return (
 		<div className={styles.pokemonGrid}>
@@ -40,10 +42,9 @@ export default function PokemonGrid(props) {
 							viewBox='0 0 24 24'
 							fill='none'
 							stroke='currentColor'
-							stroke-width='2'
-							stroke-linecap='round'
-							stroke-linejoin='round'
-							class='icon icon-tabler icons-tabler-outline icon-tabler-search'>
+							strokeWidth='2'
+							strokeLinecap='round'
+							strokeLinejoin='round'>
 							<path stroke='none' d='M0 0h24v24H0z' fill='none' />
 							<path d='M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0' />
 							<path d='M21 21l-6 -6' />
@@ -52,7 +53,7 @@ export default function PokemonGrid(props) {
 							placeholder='e.g. Pikachu'
 							value={search}
 							onChange={(e) => setSearch(e.target.value)}
-							autofocus
+							autoFocus
 						/>
 					</div>
 				</div>
@@ -69,7 +70,12 @@ export default function PokemonGrid(props) {
 								key={pokemonIndex}
 								onClick={handleSelectPokemon(pokemon.name)}
 								className={styles.pokemon}>
-								{capitalizeFirstLetter(pokemon.name)}
+								<span className={styles.pokeId}>
+									{displayId(pokemonIndex)}
+								</span>
+								<span>
+									{capitalizeFirstLetter(pokemon.name)}
+								</span>
 							</div>
 						);
 					})}
